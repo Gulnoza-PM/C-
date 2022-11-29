6763b37634ed81157057f8d37012779e6ef14361
Your_name <mgulnoza1805@mail.com>
2022-11-29 15:56:48 +0500

    task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,11 @@
-﻿// 
+﻿int a,b,c;
+a=int.Parse(Console.ReadLine());
+b=int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+int max;
+if(a>b&&a>c)max=a;
+else 
+if(b>a&&b>c) max=b;
+else 
+if(c>a&&c>b) max=c;
+Console.WriteLine(max);
