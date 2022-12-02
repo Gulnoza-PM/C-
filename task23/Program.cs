98cfbf1a2ac33ee21d3f7148ee0d941942d2c575
Your_name <mgulnoza1805@mail.com>
2022-12-02 15:38:07 +0500

    3task

---
+++ task23/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да 7 -> да 1 -> нет */ 
+//string[] date = {"pon", "vtor","sred","chet","pyat","sub","vosk"};
+
+Console.WriteLine("Please enter number");
+
+int userEntered = int.Parse( Console.ReadLine());
+
+
+if (userEntered == 6 || userEntered == 7)
+
+{
+    Console.WriteLine("da");
+
+ }
+
+ else if(userEntered>7)
+
+ {   
+        Console.WriteLine("Invalid date");
+
+ }
+
+else
+{
+   Console.WriteLine("net");
+} 
+
+
