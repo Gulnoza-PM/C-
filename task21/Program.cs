311dca729910a89e4c4ccd005fe8670c5677ba16
Your_name <mgulnoza1805@mail.com>
2022-12-02 15:12:37 +0500

    finished task 21

---
+++ task21/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.WriteLine("Please enter number");
+
+string userEntered = Console.ReadLine();
+
+
+if (userEntered.Length<3)
+{
+    Console.WriteLine("Invalid number");
+
+ }
+else
+{
+   Console.WriteLine(userEntered[2]);
+} 
+
+
+
+
+
+
+
+
+
