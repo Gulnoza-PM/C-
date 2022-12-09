841a0f5a001ab1a546efc53ea7bd811dc71e862f
Your_name <mgulnoza1805@mail.ru>
2022-12-09 22:25:33 +0500

    add correction

--- task332/Program.cs
+++ task332/Program.cs
@@ -1,7 +1,6 @@
-﻿/ Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом 14212 -> нет 
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом 14212 -> нет //
 // 12821 -> да 23432 -> //
-//Besh xonali sonni kirituvchi va uning palindrom ekanligini tekshiradigan dastur yozing 14212 -> yo'q
-// 12821 -> ha 23432 -> //
+
 Console.WriteLine("Enter number");
 int a = int.Parse(Console.ReadLine()!);
 int first = a;
