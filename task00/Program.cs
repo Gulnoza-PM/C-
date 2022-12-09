99722fd3184961f83e31c4d8eb55edd28058e414
Your_name <mgulnoza1805@mail.ru>
2022-12-09 22:10:49 +0500

    some

---
+++ task00/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+Console.WriteLine("Enter number");
+int number=int.Parse(Console.ReadLine()!);
+if(number %2 ==0) Console.WriteLine("yes");
+else
+Console.WriteLine("no");
+
+Console.WriteLine("Enter number");
+int num = int.Parse(Console.ReadLine());
+if(num<2)
+{
+Console.WriteLine("Wrong number");
+
+}
+
+int i=2;
+while(i<=num)
+ 
+
+    if(i %2==0){
+
+    Console.WriteLine(i);
+    }
+
+i+=1;
+
+
+Console.WriteLine(8 / 2 + 5 - 3 / 2);
+
+
+
+
+
+
+
+
