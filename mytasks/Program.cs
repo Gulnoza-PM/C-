23eee36cb1f8391be23110caecffaaa181ec20e7
Your_name <mgulnoza1805@mail.com>
2022-12-09 16:39:05 +0500

    added som tasks

---
+++ mytasks/Program.cs
@@ -0,0 +1,37 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// var text = "gulnoza";
+
+// Console.WriteLine(text.ToUpper());
+// var low= text.ToLower();
+// Console.WriteLine(low);
+
+
+
+var one = "Naharis";
+var two = "Mormont";
+var three = "Sand";
+var show = two[2]+three[2]+two[5]+two[3]+one[4];
+// Console.WriteLine(two[2]);
+// Console.WriteLine(three[4]);
+// Console.WriteLine(two[5]);
+// Console.WriteLine(two[3]);
+//Console.WriteLine(show);//
+Console.WriteLine($"{two[2]} {three[4]}  {two[5]} {two[3]}");
+
+
+// BEGIN (write your solution here)третий символ из первой строки;
+// второй символ из второй строки;
+// четвертый символ из третьей строки;
+// пятый символ из второй строки;
+// третий символ из второй строки.
+
+// // END
+// Console.WriteLine(two.Substring(2));
+
+// var name="Robert";
+// var surname="Baratheon";
+// var totalCount=name.Length+surname.Length;
+// Console.WriteLine($"{name} {surname} has {totalCount} characters");
+// //($"name + surname +has+totalCount+characters in his name");//
