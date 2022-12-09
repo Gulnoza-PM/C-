1511a4cc8297bf42cf6e74e699f8848f28714ae4
Your_name <mgulnoza1805@mail.com>
2022-12-09 16:40:06 +0500

    a

---
+++ fgjhf/Program.cs
@@ -0,0 +1,9 @@
+﻿// var name = "Hodor";
+
+// // BEGIN
+// var who = name.ToUpper();
+// var what = name.ToLower();
+// // END
+
+// //Console.WriteLine($"{who} says: \"{what}\"");//
+// Console.WriteLine( who + what);
