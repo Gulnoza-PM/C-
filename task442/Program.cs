8c0f3fcdac7ca565aa86437cdcb41651cf4bef4a
Your_name <mgulnoza1805@mail.ru>
2022-12-14 09:37:46 +0500

    4 lesson task

---
+++ task442/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11 / 82 -> 10 / 9012 -> 12*/
+Console.WriteLine("Enter number");
+int num= int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum of digits={SumOfDigits(num)}");
+
+
+int SumOfDigits(int number)
+
+{
+    int sum=0;
+    int result=0;
+    while(number>0)
+    {
+        sum=number%10;
+        number=number-sum;
+        result= result+sum;
+        number = number/10;
+    }
+    return result;
+
+}
+
+
+
+
+
+
+
+
+
