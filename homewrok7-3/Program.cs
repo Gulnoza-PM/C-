2a8dfbc1d52c8838a547992f99e2617a72dac4fe
Your_name <mgulnoza1805@mail.ru>
2022-12-21 20:07:44 +0500

    homework 7-3

---
+++ homewrok7-3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int[,] GetArray(int m, int n, int start, int end)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(start, end);
+        }
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMeanColumns(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int col = 0; col < array.GetLength(1); col++)
+    {
+        double sumColumn = 0;
+        for (int rows = 0; rows < array.GetLength(0); rows++)
+        {
+            sumColumn += array[rows, col];
+        }
+        result[col] = Math.Round(sumColumn / (array.GetLength(0)), 1, MidpointRounding.ToZero);
+    }
+
+    return result;
+}
+
+int[,] array = GetArray(3, 4, 1, 100);
+ShowArray(array);
+Console.WriteLine();
+Console.Write(string.Join(" ", ArithmeticMeanColumns(array)));
