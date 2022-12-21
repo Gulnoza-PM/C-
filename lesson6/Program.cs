b25a45faf576afe7bd3b677b5ceba8dddfcded9c
Your_name <mgulnoza1805@mail.ru>
2022-12-21 11:59:54 +0500

    bugfix

---
+++ lesson6/Program.cs
@@ -0,0 +1,59 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+Console.Write("Напишите сколько чисел хотите ввести: ");
+int Input=int.Parse(Console.ReadLine()!);
+
+void getInputCount(int userInput)
+{
+int count=0;
+for(int i=1;i<=userInput;i++)
+{
+    System.Console.Write($"Введите номер  {i}ый: ");
+    int num = int.Parse(Console.ReadLine()!);
+    if(num > 0)
+        count++;
+};
+Console.WriteLine($"Количество номеров больше 0: {count}");
+
+}
+getInputCount(Input);
+
+
+
+
+
+
+//  int Max(int arg1, int arg2, int arg3)
+// {
+//     int res = arg1;
+//     if(arg2> res) res = arg2;
+//     if(arg3> res) res = arg3;
+//     return res;
+// }
+// int a = 33; int b =78; int c=88; int d=71; int e = 33; int g =73; int f=83; int y=713; int r=716; 
+// int max1=Max(a, b, c);
+// int max2=Max(d, e, g);
+// int max3=Max(f, y, r);
+
+// int maxRes=Max(max1, max2, max3);
+// Console.WriteLine(maxRes);
+
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+ 
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = arr.Count(x => x > 0);
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
---
+++ lesson62/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Write("Введите точку k1: ");
+double K1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите точку b1: ");
+double B1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите точку k2: ");
+double K2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите точку b2: ");
+double B2 = double.Parse(Console.ReadLine()!);
+
+double x = GetX(B2, B1, K1, K2);
+double y = GetY(K1, x, B1);
+
+x = GetCube(x);
+y = GetCube(y);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+double GetX(double B2, double B1, double K1, double K2)
+{
+    double result = (B2 - B1) / (K1 - K2);
+    return result;
+}
+
+double GetY(double K1 ,double x,  double B1)
+{
+    double result = K1 * x + B1;
+
+    return result;
+}
+
+double GetCube(double num)
+{
+    return Math.Round(num, 3);
+}
