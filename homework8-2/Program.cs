6da668e8e4238388b917cad153794374d2e34a0b
Your_name <mgulnoza1805@mail.ru>
2022-12-26 15:39:10 +0500

    exam task

---
+++ homework8-2/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,]table=new int[2,5];
+//Генерируем рандомные числы
+void FillTableRandm(int[,] array)
+{
+
+    for(int i=0; i<array.GetLength(0); i++)
+     {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // Console.Write($"Сгенирированные числы {array [i,j]}");
+            array[i, j] = new Random().Next(1, 10);
+                       
+        }
+       
+             
+    }
+}
+FillTableRandm(table); //Вызов 
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // Console.Writeline ("generet: ");
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(table);//  print 123
+int minRow=0;
+ int GetMinRowsum(int[,] array)
+ {
+    int result=0;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int counter=0;
+        for (int j = 0; j < array.GetLength(i); j++)
+        {
+            counter +=array[i,j];
+        }
+        if(result<counter)
+        {
+            result=counter;
+            minRow=i+1;
+        }
+
+    }
+    return result;
+ }
+int res =GetMinRowsum(table);
+Console.WriteLine($"Минимальные элементы находится в : {minRow}");
+Console.WriteLine($"Сумма элементов: {res}");
+
+// SumLineElements(array, 0);
+// {
+// int tempSumLine = SumLineElements(array, i);
+//   if (sumLine > tempSumLine)
+//   {
+//     sumLine = tempSumLine;
+//     minSumLine = i;
+//   }
+// }
+// int minSumLine = 0;
+// int sumLine = SumLineElements(array, 0);
+// for (int i = 1; i < array.GetLength(0); i++)
+// {
+  
+// }
+
+// Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+// int SumLineElements(int[,] array, int i)
+// {
+//   int sumLine = array[i,0];
+//   for (int j = 1; j < array.GetLength(1); j++)
+//   {
+//     sumLine += array[i,j];
+//   }
+//   return sumLine;
+// }
+
+// Print(SumLineElements);
---
+++ homewrok8-3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int[,]table=new int[4, 4];
+//Генерируем рандомные числы
+void FillMatrixRandm(int[,] array)
+{
+
+    for(int i=0; i<array.GetLength(0); i++)
+     {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // Console.Write($"Сгенирированные числы {array [i,j]}");
+            array[i, j] = new Random().Next(1, 10);
+                       
+        }
+       
+             
+    }
+}
+FillMatrixRandm(table); //Вызов 
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // Console.Writeline ("generet: ");
+            Console.Write(array[i, j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintMatrix(table);//  print 123
+
+
+int[,] MultMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] result = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(1); k++)
+            {
+                result[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return result;
+}
+int[,] res=MultMatrix(table);
+//Console.WriteLine($"Минимальные элементы находится в : {minRow}");
+Console.WriteLine($"Сумма элементов: {res}");
