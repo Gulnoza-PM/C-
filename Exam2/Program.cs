68169f61e8cea2b3d22789663391e8fb1c594914
Your_name <mgulnoza1805@mail.ru>
2022-12-26 15:53:16 +0500

    exam 2 task

---
+++ Exam2/Program.cs
@@ -0,0 +1,23 @@
+﻿// адайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// int n, m, 
+Console.Write("Введи значение от: ");
+int N= int.Parse(Console.ReadLine()!);
+Console.Write("Введи значение до: ");
+int M = int.Parse(Console.ReadLine()!);
+
+void Numbers(int n, int m)
+{
+    int s=0;
+for (int i = n; i <= m; i++)
+{
+    s += i;
+}
+    
+Console.WriteLine(s);
+}
+
+Numbers(N,M);
