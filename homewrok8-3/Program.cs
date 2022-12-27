cbde69270dd9766cefb6bdd6c7b5922be4c234b1
Your_name <mgulnoza1805@mail.ru>
2022-12-27 12:52:51 +0500

    8 HW

--- homewrok8-3/Program.cs
+++ homewrok8-3/Program.cs
@@ -5,53 +5,71 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-int[,]table=new int[4, 4];
-//Генерируем рандомные числы
-void FillMatrixRandm(int[,] array)
-{
+//Console.WriteLine($"Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.\n\nСразу зададим матрицу, которую можно перемножить, т.е. количество столбцов первой равно количеству строк второй");
+//Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int line = UserInputNumbers("Введите количество строк 1-й матрицы: ");
+int columCount1 = UserInputNumbers("Введите  количество 1-й матрицы (и строк 2-й): ");
+int columCount2 = UserInputNumbers("Введите  количество 2-й матрицы: ");
+int range = UserInputNumbers("Введите диапазон чисел: от 1 до ");
 
-    for(int i=0; i<array.GetLength(0); i++)
-     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            // Console.Write($"Сгенирированные числы {array [i,j]}");
-            array[i, j] = new Random().Next(1, 10);
-                       
-        }
-    }
-}
-FillMatrixRandm(table); //Вызов 
+int[,] Martrix1 = new int[line, columCount1];
+CreateArray(Martrix1);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(Martrix1);
 
-void PrintMatrix(int[,] array)
+int[,] Martrix2 = new int[columCount1, columCount2];
+CreateArray(Martrix2);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(Martrix2);
+
+int[,] resultMatrix = new int[line,columCount2];
+
+MultiplyMatrix(Martrix1, Martrix2, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] Martrix1, int[,] Martrix2, int[,] resultMatrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            // Console.Writeline ("generet: ");
-            Console.Write(array[i, j]+ " ");
-        }
-        Console.WriteLine();
+      int sum = 0;
+      for (int k = 0; k < Martrix1.GetLength(1); k++)
+      {
+        sum += Martrix1[i,k] * Martrix2[k,j];
+      }
+      resultMatrix[i,j] = sum;
     }
+  }
 }
-PrintMatrix(table);//  print 123
 
-
-int[,] MultMatrix(int[,] matrix1, int[,] matrix2)
+int UserInputNumbers(string input)
 {
-    int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-    for (int i = 0; i < result.GetLength(0); i++)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            for (int k = 0; k < result.GetLength(1); k++)
-            {
-                result[i, j] += matrix1[i, k] * matrix2[k, j];
-            }
-        }
-    }
-    return result;
+  Console.Write(input);
+  int show = int.Parse(Console.ReadLine()!);
+  return show;
 }
-int res=MultMatrix(table);
-//Console.WriteLine($"Минимальные элементы находится в : {minRow}");
-Console.WriteLine($"Сумма элементов: {res}");
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
